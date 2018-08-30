5d0fe361f23c9ccc3d78f5515392dbcd1b8f49ac
Chelsea Kent <chelsea.kent@gmail.com>
2018-08-30 08:22:28 -0700

    morning CSS updates

--- scripts/index.js
+++ scripts/index.js
@@ -9,3 +9,18 @@ function main() {
 }
 
 $(main);
+
+// //FOR MY FORMS - JQuery library edit extends library to give me an object with
+// $.fn.extend({
+//   serializeJson: function() {
+//     const formData = new FormData(this[0]);
+//     const obj = {};
+//     formData.forEach((val, name) => (obj[name] = val)); //FormData only has .forEach available to it
+//     return JSON.stringify(obj);
+//   }
+// });
+
+// $('#form').submit(e => {
+//   e.preventDefalut();
+//   console.log($(e.target).serializeJson()); // call my new JQuery function on this form when submitted
+// });
